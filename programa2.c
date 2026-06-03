1fdafff47bdc35531a1527ac7f82f0d68647b079
lzinnn <luiscarlosvescovi@gmail.com>
2026-06-03 17:00:25 -0300

    03/06/2026

---
+++ programa2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main()
+{
+ double raio =0;
+ double area =0;
+ double pi= 3.14159;
+ scanf("%lf", &raio);
+ area = pi * raio * raio;
+ printf("A=%.4lf\n", area);
+ 
+ return (0);
+ 
+}	
