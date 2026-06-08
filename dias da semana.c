665a3bd15576025c023a72f7af5d908c6e1e11d0
lzinn <luizcarlosvescovi@gmail.com>
2026-06-08 16:59:14 -0300

    08/06/2026

---
+++ dias da semana.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+	int dia = 0;
+	printf("farvor informe um numero\n");
+	scanf("%d", &dia);
+	switch(dia);
+	{
+    case 1:
+	      printf("0 numero iformado represeta domingo\n");
+	break;
+	case 2:
+	printf("o numero iformado repesenta segunda-feira\n");
+	 case 3:
+	      printf("0 numero iformado represeta terca-feira\n");
+	break;
+	 case 4:
+	      printf("0 numero iformado represeta quarta-feira\n");
+	break;
+	 case 5:
+	      printf("0 numero iformado represeta quita-feira\n");
+	break;
+	 case 6:
+	      printf("0 numero iformado represeta sexta-feira\n");
+	break;
+	 case 7:
+	      printf("0 numero iformado represeta sabado\n");
+	break;
+	default:
+	      printf(" O numero informado nao representa um dia da semana\n");
+	}
+		  return(0);
+}	 
