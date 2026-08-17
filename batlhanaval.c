52960d9477d384fa505618a05c9b018c55137ed6
Manupires1 <emanuelli.pires@aluno.senai.br>
2026-08-17 16:15:46 -0300

    prova02

---
+++ batlhanaval.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+{
+
+//login do jogador 
+typedef struct
+char nome[50]
+int tentativas
+
+
+//declarcao de constantes
+const int tam = 8
+const int navios = 3
+
+
+// prototipos das funcoes 
+void inicializarTabuleiro(char tabuleiro[8][8]);
+void mostrarTabuleiro(char tabuleiro[8][8]);
+void posicionarNavios(char tabuleiro[8][8]);
+int realizarAtaque(char tabuleiro[8][8], int (desturidos));
+void salvarResultado(jogador);
+void lerResultado();
+
+//tableiro
+void preenchermatiz (int matriz[8][8], int valor){
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            matriz valor[i][j]
+
+        }
+    }
+}
+
+
+//posisinar navios
+void posicionarNavios
+    struct n (1,2)
+
+    struct n  (3,5)
+
+    struct n  (6,1)
+
+}
+
+// exibir o tabuleiro
+for (int i=0 i <tam; i++);{
+    for (int j=0 j <tam; j++);{
+        tableiro[j][i]possuinavio=0;
+        tableiro[j][i]atinguido=0;
+
+    }
+}
+
+
+
+
+//Solicitar Dados do Jogador
+printf(===== BATALHA NAVAL =====);
+printf(informe seu nome );
+
+
+//fluxo principal
+//realizar ataque
+printf(infor o local do ataque );
+printf(linha);
+printf(coluna);
+//validar linha e coluna 
